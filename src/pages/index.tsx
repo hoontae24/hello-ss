e42cbf0228d0a17945bace9d82968cf177a0e7b6
hoontae24 <kadr8941@gmail.com>
2021-12-30 01:02:29 +0900

    feat: API 응답 기반 infinite data fetch 로직 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,18 +3,43 @@ import { GetServerSideProps, NextPage } from "next";
 import MainTabView from "@/components/views/MainTabView";
 import { getMainTabByQueryValue, MainTab } from "@/consts/main-tab";
 import { getQueryParamKey, QueryParam } from "@/consts/query-param";
+import { useRelayStore } from "@/hooks/relay-store";
 
 interface PageProps {
   mainTab: MainTab;
 }
 
+const API_URL =
+  "https://gist.githubusercontent.com/styleshare-frontend/18089356186790c942c75e6dd5f90b90/raw/40517fc4a027233a03c6b220a2b069e1be7c7c39/goods-response-1.json";
+const API_URL2 =
+  "https://gist.githubusercontent.com/styleshare-frontend/1cd2be4351cd7efe64aab3d015f2bf27/raw/7bf95b3d4833a52cdb8fa78efe8b28f278891e30/goods-response-date-1.json";
+
 const Page: NextPage<PageProps> = (props) => {
   const { mainTab } = props;
+
+  const { data, sprint, depleted } = useRelayStore({
+    initialFetchUrl: API_URL2,
+    fetcher: async (url: string) => {
+      const res = await window.fetch(url);
+      return res.json() as Promise<{
+        data: { id: string }[];
+        paging?: { next?: string };
+      }>;
+    },
+    next: (data) => data?.paging?.next ?? null,
+  });
+
+  const flattenData = data?.flatMap((data) => data.data);
+
+  console.log(flattenData);
 
   return (
     <>
       <header>
         <MainTabView mainTab={mainTab} />
+        <div>{flattenData?.length}</div>
+        <div>depleted: {String(depleted)}</div>
+        <button onClick={sprint}>GOGO</button>
       </header>
     </>
   );
--- src/typings/utils.d.ts
+++ src/typings/utils.d.ts
@@ -1,2 +1,7 @@
 /** 객체, 배열의 값의 타입만 추출하는 제너릭 타입 함수 */
-export type ValueOf<T> = T extends ReadonlyArray<infer ElementType> ? ElementType : T[keyof T];
+export type ValueOf<T> = T extends ReadonlyArray<infer ElementType>
+  ? ElementType
+  : T[keyof T];
+
+/** 제너릭 타입 Value에 대하여 Promise 또는 Value 타입의 Union */
+export type PromiseOrValue<Value> = Value | Promise<Value>;
