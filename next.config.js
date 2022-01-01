eeffc75331021caf5c3cefab2ffbd2bff673e12b
hoontae24 <kadr8941@gmail.com>
2022-01-01 21:27:25 +0900

    config: svg 관련 패키지 및 설정 추가

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ module.exports = {
   images: {
     domains: [process.env.NEXT_IMAGE_HOST1],
   },
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
