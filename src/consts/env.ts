185c6ab5037b7e8ef7624a19ec0f71d188645ffe
hoontae24 <kadr8941@gmail.com>
2022-01-01 22:32:37 +0900

    feat: 추천 컴포넌트 API_URL을 .env로 등록

--- src/consts/env.ts
+++ src/consts/env.ts
@@ -1,4 +1,5 @@
 const env = {
+  /** 과제 실행 환경에서는 API_URL이 고정이므로 `.env`로 지정하지 않아도 동작하도록 fallback을 주입함.  */
   get API_URL_FOR_POPULARITY() {
     const fallback =
       "https://gist.githubusercontent.com/styleshare-frontend/18089356186790c942c75e6dd5f90b90/raw/40517fc4a027233a03c6b220a2b069e1be7c7c39/goods-response-1.json";
@@ -8,6 +9,11 @@ const env = {
     const fallback =
       "https://gist.githubusercontent.com/styleshare-frontend/1cd2be4351cd7efe64aab3d015f2bf27/raw/7bf95b3d4833a52cdb8fa78efe8b28f278891e30/goods-response-date-1.json";
     return process.env.NEXT_PUBLIC_API_URL_FOR_LATEST ?? fallback;
+  },
+  get API_URL_FOR_REVIEW_SECTION() {
+    const fallback =
+      "https://gist.githubusercontent.com/styleshare-frontend/ddc456ec9b9486b6097b61d409edc534/raw/cca24042520fab95fd6e80412eee36cc87da5c2e/goods-review-components.json";
+    return process.env.NEXT_PUBLIC_API_URL_FOR_REVIEW_SECTION ?? fallback;
   },
 } as const;
 
